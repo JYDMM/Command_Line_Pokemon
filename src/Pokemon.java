3ebf0592b5a53f844a4e72e7934113fe23e67d05
aliyaalsafa <55393747+aliyaalsafa@users.noreply.github.com>
2019-12-10 09:30:09 -0500

    hgdhhd

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,5 +1,23 @@
 public class Pokemon {
-    public Pokemon(int number, String Name, String Level, String XP) {
+    private int number;
+    private String Name;
+    private String Level;
+    private String XP;
+    private int type;
+    private int HP;
+    private Move move1;
 
+    public Pokemon(int number, String Name, String Level, String XP, int type, int HP, Move move1) {
+        this.number = number;
+        this.Name = Name;
+        this.Level = Level;
+        this.XP = XP;
+        this.type = type;
+        this.HP = HP;
+        this.move1 = move1;
     }
+
+    
+
+
 }
