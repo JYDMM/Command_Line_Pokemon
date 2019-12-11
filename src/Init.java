a48dad945bf26d67dfefc860a0db831257383c70
John Wallar <johnwallar@gmail.com>
2019-12-11 11:57:05 -0500

    Helped Pyam out a bit with init read code

--- src/Init.java
+++ src/Init.java
@@ -7,9 +7,32 @@ public class Init {
         File file =
                 new File("C:\\Users\\Creep\\Documents\\Pokemon\\save.txt");
         Scanner sc = new Scanner(file);
+        String line;
+        while (sc.hasNextLine()) {
+            line = sc.nextLine();
+            System.out.println(line);
+            if (line.contains("//")) {
+                line = sc.nextLine();
+            } else {
+                if (line.contains("# Pokemon: ")) {     // Tests if start of new pokemon
+                    String name = line.substring(11);   // Takes everything else on that line as the name of pokemon
+                    while ( ! line.contains("# Pokemon: ") || ! line.contains("#+-----------------------+#")) {
+                        line = sc.nextLine();
+                        if (line.contains("Number: ")) {
+                            int number = Integer.parseInt(line.replaceAll("[\\D]", "")); // Pulls the integer from that line
+                        } else if (line.contains("Type: ")) {
+                            int type = Integer.parseInt(line.replaceAll("[\\D]", "")); // Pulls the integer from that line
+                        } else if (line.contains("Move: ")) {
 
-        while (sc.hasNextLine())
-            System.out.println(sc.nextLine());
+                        }
+                    }
+                    Pokemon
+
+                        // ALL CODE SHOULD BE INSIDE THE ELSE STATEMENT
+                }
+            }
+        }
+
     }
     Scanner pokemonLoader = new Scanner(System.in);
 
