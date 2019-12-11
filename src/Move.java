8f66739e08c1c67c2e7caf736ea94cf134a4de95
John Wallar <johnwallar@gmail.com>
2019-12-11 09:33:33 -0500

    Added move and type code

--- src/Move.java
+++ src/Move.java
@@ -3,33 +3,37 @@ import java.util.*;
 
 public class Move {
     private String name;
-    private String type;
+    private int type;
     private boolean physical;
     private int power;
     public static void init() {
-        boolean waterSupereffective[] = {false,false,false,false,true,false,false,false,true,false,false,false,false,true,false};
-        boolean waterInffective[] = {false,true,false,false,false,false,false,true,false,false,false,false,false,true,true};
-        boolean grassSupereffective[] = {false,false,false,false,true,false,false,false,true,false,false,false,false,true,true};
-        //                                 1     2     3     4     5    6     7     8     9   10    11    12    13    14   15
-        boolean grassInffective[] = {false,true,false,false,true,true,false,true,false,false,false,true,false,true,false};
-        //                            1     2     3     4     5    6   7     8     9   10    11    12    13    14   15
-
-        //String types[] = {1"bug",2"dragon", 3"electric", 4"fighting",5"fire", 6"flying", 7"ghost",
-        //        8"grass", 9"ground",10"ice",11"normal",12"poison",13"psychic",14"rock",15"water"};
 
     }
-    public Move(int indexNumber, String name, String type, boolean physical, int power) {
+
+    public Move(int indexNumber, String name, int type, int power) {
         this.name = name;
         this.physical = physical;
         this.power = power;
     }
 
     public static int damageDone(Pokemon attacker, Move moveUsed, Pokemon beingAttacked) {
-        if (beingAttacked.type.equals("ghost") && moveUsed.physical == true) {
-            return 0;
-        }
+        // double lvlAttackMultiplier = attacker.Level() * 0.05 + 1;
+        double typeAttackMultiplier = Type.Effectiveness(moveUsed,beingAttacked);
 
-        if ()
+
+        return (int) ( moveUsed.Power() * typeAttackMultiplier);
+
     }
 
+    public String Name() {
+        return name;
+    }
+
+    public int Type() {
+        return type;
+    }
+
+    public int Power() {
+        return power;
+    }
 }
