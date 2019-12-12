e8a3431e67cc99696607779b2f97c55f98dbea94
PyamPendleton <ppendleton2022@specialmusicschool.org>
2019-12-12 13:29:43 -0500

    worked on chooseteam

--- src/Battle.java
+++ src/Battle.java
@@ -1,2 +1,9 @@
 public class Battle {
+    public static void main(String[] args) {
+//        while(player1Poke1.hp() > 0) {
+
+//        }
+
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        //Test for config file
 
         TitleScreen.Start(false);
-        System.out.println();
     }
 }
