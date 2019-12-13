d0bae246e64b920ff43f6d4bcb9f2d52eb9bc877
PyamPendleton <ppendleton2022@specialmusicschool.org>
2019-12-13 11:03:50 -0500

    worked on chooseteam

--- src/ChoseTeam.java
+++ src/ChoseTeam.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ChoseTeam extends Init {
-
+//<<<<<<< Updated upstream
     private Pokemon player1Poke1;
     private Pokemon player1Poke2;
     private Pokemon player1Poke3;
@@ -39,7 +39,7 @@ public class ChoseTeam extends Init {
     public Pokemon getPlayer2Poke3() {
         return player2Poke3;
     }
-
+//=======
     public void team(){
         Scanner teamSelect = new Scanner(System.in);
 
@@ -47,6 +47,8 @@ public class ChoseTeam extends Init {
         System.out.println("(C)harmander     (S)quirtle     (B)ulbasaur");
 
         char selector1 = teamSelect.nextLine().charAt(0);
+
+// Make player1Poke1 switch to player1Poke2 after choosing first pokemon
 
         if (selector1 == 'C' || selector1 == 'c'){
             System.out.println("Number: " + Charmander.Number());
@@ -57,17 +59,36 @@ public class ChoseTeam extends Init {
             char selector2 = teamSelect.nextLine().charAt(0);
             if (selector2 == 'Y') {
                 player1Poke1.setNumber(Charmander.Number());
+                player1Poke1.setName(Charmander.Name());
+                player1Poke1.setType(Charmander.Type());
+                player1Poke1.setHP(Charmander.HP());
             }
         } else if (selector1 == 'S' || selector1 == 's'){
             System.out.println("Number: " + Squirtle.Number());
             System.out.println("Name: " + Squirtle.Name());
             System.out.println("Type: " + Squirtle.Type());
             System.out.println("HP: " + Squirtle.HP());
+            System.out.println("Select?  Y / N");
+            char selector2 = teamSelect.nextLine().charAt(0);
+            if (selector2 == 'Y') {
+                player1Poke1.setNumber(Squirtle.Number());
+                player1Poke1.setName(Squirtle.Name());
+                player1Poke1.setType(Squirtle.Type());
+                player1Poke1.setHP(Squirtle.HP());
+            }
         } else if (selector1 == 'B' || selector1 == 'b'){
             System.out.println("Number: " + Bulbasaur.Number());
             System.out.println("Name: " + Bulbasaur.Name());
             System.out.println("Type: " + Bulbasaur.Type());
             System.out.println("HP: " + Bulbasaur.HP());
+                System.out.println("Select?  Y / N");
+                char selector2 = teamSelect.nextLine().charAt(0);
+                if (selector2 == 'Y') {
+                    player1Poke1.setNumber(Bulbasaur.Number());
+                    player1Poke1.setName(Bulbasaur.Name());
+                    player1Poke1.setType(Bulbasaur.Type());
+                    player1Poke1.setHP(Bulbasaur.HP());
+                }
         } else team();
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        TitleScreen.Start(false);
+        TitleScreen.Start();
     }
 }
